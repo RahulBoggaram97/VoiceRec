fbf59235c73fd8f969d056d59b5efe6d1190496e
RahulBoggaram97 <rahulboggaram913@gmail.com>
2022-03-21 12:58:40 +0530

    FirstCommit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D bird;
+
+    int score = 0;
+
+    bool dead = false;
+
+	void Start ()
+    {
+        bird = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown("space") && !dead)
+        {
+            bird.velocity = new Vector2(0,8.5f);
+        }
+	}
+}
